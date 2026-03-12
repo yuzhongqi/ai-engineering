79c350dee5314255393cc38bb32a0ee609903daf
zhongqiyu <zhongqiyu@microsoft.com>
2026-03-12 14:24:07 +0800

    set up demo

---
+++ AIEngineering/AIEngineering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIEngineering
+//
+//  Created by zhongqi yu on 2026/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AIEngineering/AIEngineeringTests/AIEngineeringTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIEngineeringTests.swift
+//  AIEngineeringTests
+//
+//  Created by zhongqi yu on 2026/3/12.
+//
+
+import Testing
+@testable import AIEngineering
+
+struct AIEngineeringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
